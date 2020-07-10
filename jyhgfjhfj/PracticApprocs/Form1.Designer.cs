3b52210c76e86d526d705ded122a70244c730152
Mikhov1112 <68114727+Mikhov1112@users.noreply.github.com>
2020-07-10 17:48:48 +0300

    Add files via upload

---
+++ jyhgfjhfj/PracticApprocs/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Аpproximation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(738, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(212, 529);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "X";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Y";
+            this.Column2.Name = "Column2";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(794, 532);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "аппроксимир-овать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button2.Location = new System.Drawing.Point(794, 583);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(63, 572);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Lavender;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(-1, 0);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "точки";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "аппроксимация";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(733, 529);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(632, 569);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Info;
+            this.label2.Location = new System.Drawing.Point(567, 572);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Xmax";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(950, 646);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ jyhgfjhfj/PracticApprocs/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Аpproximation
+{
+    public partial class Form1 : Form
+    {
+        private List<float> x;
+        private List<float> y;
+        private List<float> y1;
+        private List<float> x1;
+        private float SumXY, SumX2, SumX, SumY;
+        private float a, b;
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int width = ClientSize.Width - dataGridView1.Width;
+            int height = ClientSize.Height;
+            g.DrawLine(new Pen(Color.Black), dataGridView1.Width + 20, height / 2 + dataGridView1.Height / 2, dataGridView1.Width - 20 + width, height / 2 + dataGridView1.Height / 2);
+            g.DrawLine(new Pen(Color.Black), 30 + dataGridView1.Width, 20, 30 + dataGridView1.Width, height - 20);
+            for (int i = 0; i < x.Count; i++)
+            {
+                g.FillEllipse(new SolidBrush(Color.Black), x[i] + dataGridView1.Width + 30-2, - y[i] + height / 2 + dataGridView1.Height / 2 - 2, 4, 4);
+            }
+            g.DrawLine(new Pen(Color.Red), dataGridView1.Width+20,height-20-b,dataGridView1.Width+20+dataGridView1.Width-20,height-20-a*(dataGridView1.Width + 20 + dataGridView1.Width - 20)-b);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            x.Clear(); y.Clear();
+            x1.Clear(); y1.Clear();
+            dataGridView1.Columns.Clear(); dataGridView1.ColumnCount = 2;
+            chart1.Series[0].Points.DataBindXY(x, y);
+            chart1.Series[1].Points.DataBindXY(x1, y1);
+            label1.Text = null;
+            textBox1.Text = "0";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            x = new List<float>();
+            y = new List<float>();
+            x1 = new List<float>();
+            y1 = new List<float>();
+            dataGridView1.ColumnCount = 2;
+            label1.Text = null;
+            textBox1.Text = "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x.Clear();y.Clear();
+            x1.Clear();y1.Clear();
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                x.Add((float)Convert.ToDouble(dataGridView1.Rows[i].Cells[0].Value));
+                y.Add((float)Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value));
+            }
+            Appoximation();
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            if (textBox1.Text == "0")
+                chart1.ChartAreas[0].AxisX.Maximum = 100;
+            else
+                chart1.ChartAreas[0].AxisX.Maximum = Convert.ToInt32(textBox1.Text);
+            x1.Add(0);x1.Add(Convert.ToInt32(chart1.ChartAreas[0].AxisX.Maximum));
+            y1.Add(Convert.ToInt32(b));y1.Add(Convert.ToInt32(a * x1[1] + b));
+            chart1.Series[0].Points.DataBindXY(x, y);
+            chart1.Series[1].Points.DataBindXY(x1, y1);
+            if (b < 0)
+                label1.Text = "y = " + a + "x - " + Math.Abs(b);
+            else
+                label1.Text = "y = " + a + "x + " + b;
+
+
+        }
+        private void Appoximation()
+        {
+            SumX = 0;SumY = 0;SumXY = 0;SumX2 = 0;
+            for (int i = 0; i < x.Count; i++)
+            {
+                SumX += x[i];
+                SumY += y[i];
+                SumXY += x[i] * y[i];
+                SumX2 += x[i] * x[i];
+            }
+            a = (float)(x.Count * SumXY - SumX * SumY) / (float)(x.Count * SumX2 - SumX * SumX);
+            b = (float)(SumY - a * SumX) / (float)x.Count;                               
+        }
+    }
+}
